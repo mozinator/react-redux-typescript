91869295b39f48371f1c0e73632469abf53de377
Fabian Aussems <fabianaussems@gmail.com>
2017-07-26 20:22:10 +0200

    updated es5

---
+++ es5/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @author Piotr Witek <piotrek.witek@gmail.com> (http://piotrwitek.github.io)
+ * @copyright Copyright (c) 2016 Piotr Witek
+ * @license MIT
+ */
+export * from './types';
+export * from './helpers';
+export * from './create-action-creator';
+export * from './returntypeof';
